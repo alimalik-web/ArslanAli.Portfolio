cc73cf67ccd73cb32feb17a370db1447bce5ec94
alimalik-web <arslana084@gmail.com>
2026-05-06 15:49:16 +0500

    feat: added vercel analytics component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,3 +25,16 @@ export default function Home() {
     </>
   );
 }
+import { Analytics } from '@vercel/analytics/react';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
+}
+
+export default MyApp;
+
